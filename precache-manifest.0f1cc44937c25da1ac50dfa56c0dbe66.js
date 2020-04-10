73fb5aea1914fdbc3ee429a5afbffecfc3cf4f8a
sureshIween <58979944+sureshIween@users.noreply.github.com>
2020-04-10 13:25:00 +0530

    Updates

---
+++ precache-manifest.0f1cc44937c25da1ac50dfa56c0dbe66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8eb7ef29da17d5d5e78a6555a596a8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f819212f3a755b51efbc",
+    "url": "/monsters-rolodex/static/css/main.be7c3b93.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f819212f3a755b51efbc",
+    "url": "/monsters-rolodex/static/js/main.21f906a8.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
